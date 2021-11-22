ba028fb362f7dddf70cf4e7025baf11e51d8943a
Rodrigo Weilg <56870068+rweilg@users.noreply.github.com>
2021-11-22 23:20:44 +0100

    branding with name

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Resizable Tabs</title>
+        <title>Resizable Tabs by Rodrigo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
